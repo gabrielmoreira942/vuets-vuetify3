62397adf24023ca63d4f99b1ec3c7cc62d88da8e
gabriel <maboroshiken@gmail.com>
2024-03-31 00:10:08 -0300

    first commit

---
+++ src/pages/teste.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>PAI</h1>
+    <v-text-field v-model="age"></v-text-field>
+    <persons-list :persons="persons" v-model="age"></persons-list>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import { PersonInterface } from "../types/Person";
+let persons = reactive<PersonInterface[]>([
+  {
+    id: 5,
+    age: 3,
+    name: "gabs",
+  },
+  {
+    id: 3,
+    age: 3,
+    name: "gabs",
+  },
+]);
+let age = ref<number>(24)
+
+</script>
---
+++ src/types/Person.ts
@@ -0,0 +1,5 @@
+export interface PersonInterface {
+  id: number,
+  age: number,
+  name: string,
+}
